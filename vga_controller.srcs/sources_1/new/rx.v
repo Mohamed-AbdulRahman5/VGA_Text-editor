8224086bb3e3fb83dcff1c6cac22fc84230e4459
Mohamed-AbdulRahman5 <114084920+Mohamed-AbdulRahman5@users.noreply.github.com>
2023-07-04 04:08:27 +0300

    rx.v

--- vga_controller.srcs/sources_1/new/rx.v
+++ vga_controller.srcs/sources_1/new/rx.v
@@ -98,68 +98,68 @@ module rx#(
     
     // next logic
     always@(*) begin
-     state_next <= state_reg;
-     parity_next <= parity_reg;
-           s_next <= s_reg;
-           n_next <= n_reg;
-           b_next <= b_reg;
-           rx_done <= 1'b0; 
-           fr_err <= 1'b0;
-           rx_err <= 1'b0;
+     state_next = state_reg;
+     parity_next = parity_reg;
+           s_next = s_reg;
+           n_next = n_reg;
+           b_next = b_reg;
+           rx_done = 1'b0; 
+           fr_err = 1'b0;
+           rx_err = 1'b0;
          
      case(state_reg)
         idle:    
           if(~rx) begin        
-            state_next <= start;
-            parity_next<=0;
-             s_next<=0;
+            state_next = start;
+            parity_next = 0;
+             s_next = 0;
                     end
-            else      state_next <= state_reg;    
+            else      state_next = state_reg;    
           
         start:
           if(s_tick) 
             if(s_reg==7) 
             if(~rx) begin
-               s_next <=0;
-               n_next<=0;
-               state_next <= data;
+               s_next =0;
+               n_next =0;
+               state_next = data;
                end
-            else   state_next <= idle;
-            else s_next <=s_reg+1;
-            else  state_next <= state_reg;
+            else   state_next = idle;
+            else s_next =s_reg+1;
+            else  state_next = state_reg;
         data:    
           if(s_tick) begin    
             if(s_reg==15) begin 
              if(n_reg==D_bits-1)
               begin
-                rx_err <= ~(parity_reg== rx);
-                 n_next<=0;
-                 s_next<=0;
-                 state_next <=stop;
+                rx_err = ~(parity_reg== rx);
+                 n_next =0;
+                 s_next =0;
+                 state_next =stop;
                     end
-             else begin    s_next<=0;
-                 b_next <= {rx,b_reg[D_bits-2:1]};//shifting right  
-                 parity_next <= parity_reg ^ rx;  
-                  n_next <=n_reg+1;  end//else for n_reg != d_bits            
+             else begin    s_next =0;
+                 b_next = {rx,b_reg[D_bits-2:1]};//shifting right  
+                 parity_next = parity_reg ^ rx;  
+                  n_next =n_reg+1;  end//else for n_reg != d_bits            
              end
-            else s_next <=s_reg+1;//else for s_reg=!15
+            else s_next =s_reg+1;//else for s_reg=!15
               end 
-              else  state_next <= state_reg;
+              else  state_next = state_reg;
         stop:
             if(s_tick)
               if(s_reg == sb_tick-1)
                begin
-              if(~rx) fr_err <= 1'b1;
-              else  fr_err <= 1'b0;
-              state_next <= idle;
-              rx_done <= 1'b1;
+              if(~rx) fr_err = 1'b1;
+              else  fr_err = 1'b0;
+              state_next = idle;
+              rx_done = 1'b1;
               
                end
              
-               else  s_next <= s_reg+1;
-               else state_next <= state_reg;
+               else  s_next = s_reg+1;
+               else state_next = state_reg;
              default:
-                           state_next <= idle;
+                           state_next = idle;
        endcase   
       end
     
